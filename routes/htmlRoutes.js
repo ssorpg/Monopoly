d07f3eb64b6f3fb23494a7d57c26016e18d3b9bc
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-07-30 23:03:07 -0700

    define route '/' on server that sends back login.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,15 +6,16 @@ const app = express.Router();
 
 // MODELS
 const Knex = require('../config/connection');
-
+const path = require('path');
 
 
 // ROUTES
 app.get('/', async (req, res) => {
     try {
-        const resUsers = await Knex('users');
+        // const resUsers = await Knex('users');
 
-        res.status(200).json(resUsers);
+        // res.status(200).json(resUsers);
+        res.sendFile(path.join(__dirname + "/../public/index/login.html"));
     }
     catch (err) {
         res.status(500).end();
